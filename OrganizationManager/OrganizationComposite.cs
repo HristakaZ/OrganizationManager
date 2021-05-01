23de14fdead7614108e3bde9c794721b2b7d2b2a
HristakaZ <hristaka2013@abv.bg>
2021-05-02 01:44:46 +0300

    Added the organization entity, alongside the employees, taking the composite design pattern into consideration.

---
+++ OrganizationManager/OrganizationComposite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationManager
+{
+    public abstract class OrganizationComponent
+    {
+        protected string name;
+        public OrganizationComponent(string name)
+        {
+            this.name = name;
+        }
+        public abstract void Add(OrganizationComponent organizationComponent);
+        public abstract void Remove(OrganizationComponent organizationComponent);
+        public abstract void Display();
+    }
+    public class OrganizationComposite : OrganizationComponent
+    {
+        private List<OrganizationComponent> organizations = new List<OrganizationComponent>();
+        public OrganizationComposite(string name) : base(name)
+        {
+
+        }
+        public override void Add(OrganizationComponent teamComponent)
+        {
+            organizations.Add(teamComponent);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Organization Name: " + this.name);
+            foreach (OrganizationComponent organization in organizations)
+            {
+                organization.Display();
+            }
+        }
+
+        public override void Remove(OrganizationComponent organizationComponent)
+        {
+            organizations.Remove(organizationComponent);
+        }
+    }
+    public class EmployeeLeaf : OrganizationComponent
+    {
+        public EmployeeLeaf(string name) : base(name)
+        {
+
+        }
+        public override void Add(OrganizationComponent teamComponent)
+        {
+            Console.WriteLine("Cannot make operations on a leaf node(Employee).");
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("Employee Name: " + this.name);
+        }
+
+        public override void Remove(OrganizationComponent teamComponent)
+        {
+            Console.WriteLine("Cannot make operations on a leaf node(Employee).");
+        }
+    }
+}
---
+++ OrganizationManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OrganizationManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TO DO: make the console app more interactive, ask the user for the input and enter input while the user wants to - 
+             probably with the state design pattern, if the user wants to write the organization's name, then the state is on,
+            if he doesn't want to, then the state is off, and based on the state, we will just cancel a while/do while loop*/
+            OrganizationComposite organization = new OrganizationComposite("Ico'S Company");
+            EmployeeLeaf employee = new EmployeeLeaf("Ico");
+            EmployeeLeaf secondEmployee = new EmployeeLeaf("Nqkuv Tam");
+            organization.Add(employee);
+            organization.Add(secondEmployee);
+            organization.Display();
+            Console.WriteLine();
+            organization.Remove(secondEmployee);
+            organization.Display();
+            Console.WriteLine();
+        }
+    }
+}
